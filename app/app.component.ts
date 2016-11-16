674bf74027558b671b787f5497b207807dee0395
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-11-16 10:35:26 +0100

    remove moduleId and serve files from both dirs in the JIT mode

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  moduleId: module.id,
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   showHeading: boolean = true;
