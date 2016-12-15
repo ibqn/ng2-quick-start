6180338e5d92375fc7d27ca4ddc0672961821a55
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-12-15 13:32:31 +0100

    switch from the json config to the js based approach to include default middleware

---
+++ aot/bs-config.js
@@ -0,0 +1,22 @@
+'use strict';
+var fallback = require('connect-history-api-fallback');
+var log = require('connect-logger');
+/*
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ */
+module.exports = {
+    injectChanges: false, // workaround for Angular 2 styleUrls loading
+    filters: ['./**/*.{html,htm,css,js}'],
+    watchOptions: {
+        ignored: 'node_modules'
+    },
+    server: ['./aot'],
+    middleware: [
+        log({ format: '%date %status %method %url' }),
+        fallback({
+            index: '/index.html',
+            htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'] // systemjs workaround
+        })
+    ]
+};
