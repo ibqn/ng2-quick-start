79a8b623dae9bebc90fd333b6ad16c34812024c9
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-12-24 13:16:20 +0100

    update packages and remove in-memory-web-api

--- systemjs.config.js
+++ systemjs.config.js
@@ -23,8 +23,7 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'rxjs':                      'npm:rxjs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
