f926dc31eb078e3c7eb91c4c4052b902067499da
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-09 17:27:55 +0100

    collect default handle warning implementation so it can be easily removed

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,33 @@
 
 'use strict';
 
-import chalk from 'chalk';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
 
 // taken from https://github.com/rollup/rollup/blob/master/bin/src/runRollup.js
+import chalk from 'chalk';
 const seen = new Set();
 if (!process.stderr.isTTY) chalk.enabled = false;
 const warnSymbol = process.stderr.isTTY ? `⚠️   ` : `Warning: `;
 const stderr = console.error.bind(console);
+const handleWarning = warning => {
+    const str = warning.toString();
+    if (seen.has(str)) return;
+    seen.add(str);
+    stderr(`${warnSymbol}${chalk.bold( warning.message )}`);
+    if (warning.url) {
+        stderr(chalk.cyan(warning.url));
+    }
+    if (warning.loc) {
+        stderr(`${warning.loc.file} (${warning.loc.line}:${warning.loc.column})`);
+    }
+    if (warning.frame) {
+        stderr(chalk.dim(warning.frame));
+    }
+    stderr('');
+};
+
 const warnFilter = /The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten/;
 
 //paths are relative to the execution path
@@ -23,27 +40,8 @@ export default {
   format: 'iife',
   onwarn: warning => {  // overwite the default warning function
       const str = warning.toString();
-
-      if (seen.has(str)) return;
       if (warnFilter.test(str)) return;
-
-      seen.add(str);
-
-      stderr(`${warnSymbol}${chalk.bold( warning.message )}`);
-
-      if (warning.url) {
-          stderr(chalk.cyan(warning.url));
-      }
-
-      if (warning.loc) {
-          stderr(`${warning.loc.file} (${warning.loc.line}:${warning.loc.column})`);
-      }
-
-      if (warning.frame) {
-          stderr(chalk.dim(warning.frame));
-      }
-
-      stderr('');
+      handleWarning(warning);
   },
   plugins: [
     nodeResolve({jsnext: true, module: true}),
