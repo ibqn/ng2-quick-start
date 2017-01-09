d5eb2de849b0273ce9d8510d7d80a25c701de3bf
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-09 16:41:50 +0100

    filter boring warning message

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
-//jshint esversion: 6
+/* jshint esversion: 6 */
 
-import rollup      from 'rollup';
+'use strict';
+
+import chalk from 'chalk';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
+
+// taken from https://github.com/rollup/rollup/blob/master/bin/src/runRollup.js
+const seen = new Set();
+if (!process.stderr.isTTY) chalk.enabled = false;
+const warnSymbol = process.stderr.isTTY ? `⚠️   ` : `Warning: `;
+const stderr = console.error.bind(console);
+const warnFilter = /The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten/;
 
 //paths are relative to the execution path
 export default {
@@ -12,6 +21,30 @@ export default {
   sourceMap: true,
   sourceMapFile: 'aot/dist/build.js.map',
   format: 'iife',
+  onwarn: warning => {  // overwite the default warning function
+      const str = warning.toString();
+
+      if (seen.has(str)) return;
+      if (warnFilter.test(str)) return;
+
+      seen.add(str);
+
+      stderr(`${warnSymbol}${chalk.bold( warning.message )}`);
+
+      if (warning.url) {
+          stderr(chalk.cyan(warning.url));
+      }
+
+      if (warning.loc) {
+          stderr(`${warning.loc.file} (${warning.loc.line}:${warning.loc.column})`);
+      }
+
+      if (warning.frame) {
+          stderr(chalk.dim(warning.frame));
+      }
+
+      stderr('');
+  },
   plugins: [
     nodeResolve({jsnext: true, module: true}),
     commonjs({
@@ -20,4 +53,3 @@ export default {
     uglify()
   ]
 };
-
