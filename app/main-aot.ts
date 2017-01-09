dd8582e46d86a888fdd029d1e0d9f1ded09d2c71
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-09 13:30:13 +0100

    enable production mode for aot compilation

--- app/main-aot.ts
+++ app/main-aot.ts
@@ -1,6 +1,8 @@
+import { enableProdMode  }     from '@angular/core';
 import { platformBrowser }    from "@angular/platform-browser";
 
 import { AppModuleNgFactory } from "../aot/app/app.module.ngfactory";
 
+enableProdMode();
 const platform = platformBrowser();
 platform.bootstrapModuleFactory(AppModuleNgFactory);
