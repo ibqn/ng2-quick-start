b910c071dab26c9d816557bdbd9c8f61ff09bee6
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-26 10:14:04 +0100

    fullfil no-inferrable-types lint rule

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  showHeading: boolean = true;
+  public showHeading = true;
   heroes: string[] = ['Magneta', 'Bombasto', 'Magma', 'Tornado'];
 
   toggleHeading() {
