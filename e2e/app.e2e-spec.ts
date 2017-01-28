a75f6daa80e147c847447ecd70aaac6779462769
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-01-28 08:30:59 +0100

    add another e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,13 +4,24 @@ import { browser, element, by } from 'protractor';
 describe('QuickStart E2E Tests', () => {
 
   const expectedMsg = 'My First Angular App';
+  const buttonText = 'Toggle Heading';
 
   beforeEach(() => {
     browser.get('');
   });
 
-  it('should display: ' + expectedMsg, () => {
+  it(`Should display: ${expectedMsg}`, () => {
     expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  });
+
+  it('Should verify that toggle button is present', () => {
+    const button = element(by.buttonText(buttonText));
+
+    expect(button.getTagName()).toBe('button');
+
+    button.click().then(() => {
+      expect(element(by.css('h1')).isPresent()).toBe(false);
+    });
   });
 
 });
