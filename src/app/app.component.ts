e58e17e62b86580ec5fc3b1d875ba42aefe3afe9
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-02-07 11:39:42 +0100

    comment must start with a space. lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  //moduleId: module.id,
+  // moduleId: module.id,
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
